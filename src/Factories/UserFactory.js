4e3f6c95db199694197c19581a9d78d26ed577e6
Wanderson Cesario de Souza <38380430+wando15@users.noreply.github.com>
2022-07-12 18:05:31 -0300

    Co-authored-by: Lucas Araraki <lucasararaki@users.noreply.github.com>

--- src/Factories/UserFactory.js
+++ src/Factories/UserFactory.js
@@ -2,15 +2,14 @@ const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
 const { Mailer } = require("../../helpers/mailer");
-const ApiError = require("../../helpers/APIError");
-const config = require("../../config/server-config");
+const config = require("../../config/server.config");
 const { UserController } = require("../controllers");
 const { UserRepository } = require("../repositories");
 const Model = require("../models");
 
 async function CreateUserFactory(req, res, next) {
     const userRepository = UserRepository(Model.User);    
-    const userController = UserController({ userRepository, bcrypt, config, ApiError });
+    const userController = UserController({ userRepository, bcrypt, config});
     
     console.warn({ userController })
 
